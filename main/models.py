2a672a0dc7e913b03b9c40acd8979c4d848efe7b
cassio <cassio.araujo.lima@gmail.com>
2024-05-10 21:24:42 -0300

    models do main

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200)
+
+class Item(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    text = models.TextField(max_length=255)
+    complete = models.BooleanField(default=False)
