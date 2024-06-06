b180c5dbafdceab70803d2dce17e1a2cd5601edc
cassio <cassio.araujo.lima@gmail.com>
2024-06-05 21:17:29 -0300

    index,show,base

--- main/views.py
+++ main/views.py
@@ -1,34 +1,43 @@
 from django.http import HttpResponse
 from main.models import Todo
+from main.models import Item
+from django.shortcuts import render, redirect
 
 def index(request):
-   todo = Todo.objects.all().first()
-   context = f"""
-   <h1>Todo`s</h1>
-   <ul>
-      <li>
-         <p>{todo}</p>
-      </li>
-   </ul>
-   """
-   return HttpResponse(context)
+    todos = Todo.objects.all()
+    return render(request, 'main/index.html', {'todos':todos})
 
-def show(request,id):
-   todos = Todo.objects.filter(id=id)
-   items = []
-   if todos:
-      for todo in todos:
-         for item in todo.items.all():
-            items.append(item)
-   return HttpResponse(f"""
-                  <ul>
-                       <li>
-                           <p>{items[0].text}</p>
-                       </li>
-                  </ul>
-                  <ul>
-                       <li>
-                           <p>{items[1].text}</p>
-                       </li>
-                  </ul>""")
-   
+def show(request, id):
+    todo = Todo.objects.filter(id=id).first()
+    itens = todo.items.all()
+    return render(request, 'main/show.html', {'name':todo.name, 'items': itens, 'todo_id': todo.id})
+
+def store(request, todo_id=None):
+    if request.method == 'POST':
+        if not todo_id:
+            name = request.POST.get('todo_name')
+            todo = Todo.objects.create(name=name)
+            return redirect('index')
+        else: 
+            todo = Todo.objects.get(id=todo_id)
+            text = request.POST.get('text')
+            todo.items.create(text=text)
+            return redirect('show', id=todo_id)
+        
+def destroy(request, todo_id=None):
+    if request.method == 'POST':
+        todo = Todo.objects.filter(id=todo_id).delete()
+    return redirect('index')
+
+def destroy_item(request, todo_id, item_id):
+    if request.method == 'POST':
+        todo = Todo.objects.get(id=todo_id)
+        todo.items.filter(id=item_id).delete()
+    return redirect('show', id=todo_id)
+
+def put(request, item_id):
+    if request.method == 'POST':
+        item = Item.objects.get(id=item_id)
+        item.complete = not item.complete
+        item.save()
+        return redirect('show', id=item.todo.id)
